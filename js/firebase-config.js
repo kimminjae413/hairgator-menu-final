9d7a8c014c850d5af67e150d2ed1a65cd92083b8
kimminjae413 <eric708@naver.com>
2025-08-19 11:01:41 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,19 +4,17 @@ const firebaseConfig = {
     authDomain: "hairgatormenu-4a43e.firebaseapp.com",
     databaseURL: "https://hairgatormenu-4a43e-default-rtdb.firebaseio.com",
     projectId: "hairgatormenu-4a43e",
-    storageBucket: "hairgatormenu-4a43e.firebasestorage.app",
+    storageBucket: "hairgatormenu-4a43e.appspot.com",
     messagingSenderId: "800038006875",
     appId: "1:800038006875:web:2a4de70e3a306986e0cf7e"
 };
 
 // Firebase 초기화
 firebase.initializeApp(firebaseConfig);
-
-// Firestore 초기화 - 새로운 방식
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-// 새로운 캐시 설정 방식 (권장)
+// 캐시 설정
 db.settings({
     cache: {
         kind: 'persistent',
@@ -25,18 +23,5 @@ db.settings({
         }
     }
 });
-
-// 구버전 브라우저를 위한 폴백
-if (!db.settings.cache) {
-    // 구버전 방식 (경고는 나지만 작동함)
-    db.enablePersistence({ synchronizeTabs: true })
-        .catch((err) => {
-            if (err.code === 'failed-precondition') {
-                console.log('Multiple tabs open, persistence disabled');
-            } else if (err.code === 'unimplemented') {
-                console.log('Browser doesn\'t support offline persistence');
-            }
-        });
-}
 
 console.log('✅ Firebase initialized with optimized cache settings');
