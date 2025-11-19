5dd6f7d2f0e47b1148a5c6ff4b9b93ff309eed5c
kimminjae413 <eric708@naver.com>
2025-11-19 10:37:53 +0900

    Update chatbot-api.js

--- netlify/functions/chatbot-api.js
+++ netlify/functions/chatbot-api.js
@@ -1139,7 +1139,7 @@ ${JSON.stringify(params56, null, 2)}
           { role: 'user', content: userPrompt }
         ],
         temperature: 0.5,
-        max_tokens: 2000
+        max_tokens: 8000
       })
     });
 
